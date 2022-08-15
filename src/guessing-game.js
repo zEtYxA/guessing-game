63bbc25bc1ca4a62c2befc41d4e921e2984393d8
zEtYxA <zmaxet@mail.ru>
2022-08-16 00:47:08 +0300

    finish guess

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,34 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
+        this.min = min;
+        this.max = max;
+        return
+        //this.min = this.min * 3;
+        //this.max = max;
 
     }
 
     guess() {
 
+        let s = Math.round((this.max - this.min) / 2) + this.min
+        this.mid = s;
+        console.log(s);
+        return s
+
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
+        this.min = this.mid;;
 
     }
 }
